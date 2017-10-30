e0d5ca5a7ffeee88151f285af77ea26bb6770db9
Cody Kasselman <ckasselman@fordham.edu>
2017-10-30 18:58:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ function getBranches(el){
 
 function displayBranches(){
   const branches = JSON.parse(this.responseText)
-  const branchList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
+  const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = branchesList
 }
