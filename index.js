57a22b8f6985fbe09e3e43fc272738cf2952eff1
Cody Kasselman <ckasselman@fordham.edu>
2017-10-30 19:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,15 +11,14 @@ function displayRepositories(event, data){
   const repoList = "<ul>" + repos.map(repo => {
     const dataUsername = 'data-username="' + repo.owner.login + '"'
     const dataRepoName = 'data-repository="' + repo.name + '"'
-    return(`
-      <li>
+    return(
+      `<li>
         <h2>${repo.name}</h2>
         <a href="${repo.html_url}">${repo.html_url}</a><br>
         <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
         <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a><br>
       </li>`
     )}).join('') + "</ul>"
-
   document.getElementById("repositories").innerHTML = repoList
 }
 
@@ -27,7 +26,6 @@ function getCommits(el){
   const repoName = el.dataset.repository
   const username = el.dataset.username
   const req = new XMLHttpRequest()
-
   req.addEventListener("load", displayCommits)
   req.open("GET", `https://api.github.com/repos/${username}/${repoName}/commits`)
   req.send()
@@ -35,8 +33,7 @@ function getCommits(el){
 
 function displayCommits(){
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul${commits.map(commit => '<li><h3>' + commit.commit.author.name +
-  ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
+  const commitsList = `<ul${commits.map(commit => '<li><h3>' + commit.commit.author.name + '(' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
 
@@ -44,7 +41,6 @@ function getBranches(el){
   const repoName = el.dataset.repository
   const username = el.dataset.username
   const req = new XMLHttpRequest()
-
   req.addEventListener("load", displayBranches)
   req.open("GET", `https://api.github.com/repos/${username}/${repoName}/branches`)
   req.send()
