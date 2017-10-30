24f1f221abbca7c71f653242b309c34acf67edfb
Cody Kasselman <ckasselman@fordham.edu>
2017-10-30 18:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 function getRepositories(){
-  let username = document.getElementById('name').value
-
   const req = new XMLHttpRequest()
-
+  let username = document.getElementById('username').value
   req.addEventListener("load", displayRepositories)
-  req.open("GET", `https://api.github.com/users/${username}/repos`)
+  req.open('GET', `https://api.github.com/users/${username}/repos`)
   req.send()
 }
 
 function displayRepositories(event, data){
   let repos = JSON.parse(this.responseText)
-  const repoList = `<ul>${repos.map(r =>
-    '<li>' +
-      r.owner.login + '<br>' +
-      r.name + '<br>' +
-      r.html_url + '<br>' +
-      '<a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)"> Get Commits</a>' + '<br>'
-      '<a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getBranches(this)"> Get Branches</a>' +
-        '</li>'
-  ).join('')}</ul>`
+  console.log(repos)
+  const repoList = "<ul>" + repos.map(repo => {
+    const dataUsername = 'data-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
+    return(`
+      <li>
+        <h2>${repo.name}</h2>
+        <a href="${repo.html_url}">${repo.html_url}</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a><br>
+      </li>`
+    )}).join('') + "</ul>"
 
   document.getElementById("repositories").innerHTML = repoList
 }
