07ad1d4c8c00e8a56aef3ea68a53bfed209360eb
Cody Kasselman <ckasselman@fordham.edu>
2017-10-30 18:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function getRepositories(){
-  let username = document.getElementById('name').value;
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", displayRepositories);
-  req.open("GET", `https://api.github.com/users/${username}/repos`);
-  req.send();
+  let username = document.getElementById('name').value
+
+  const req = new XMLHttpRequest()
+
+  req.addEventListener("load", displayRepositories)
+  req.open("GET", `https://api.github.com/users/${username}/repos`)
+  req.send()
 }
 
 function displayRepositories(event, data){
@@ -15,8 +17,9 @@ function displayRepositories(event, data){
       r.html_url + '<br>' +
       '<a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)"> Get Commits</a>' + '<br>'
       '<a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getBranches(this)"> Get Branches</a>' +
-    '</li>'
+        '</li>'
   ).join('')}</ul>`
+
   document.getElementById("repositories").innerHTML = repoList
 }
 
@@ -24,20 +27,23 @@ function getCommits(el){
   const name = el.dataset.repository
   const username = el.dataset.username
   const req = new XMLHttpRequest()
-  req.addEventListener("load", displayCommits);
-  req.open("GET", `https://api.github.com/repos/${username}/${name}/commits`);
-  req.send();
+
+  req.addEventListener("load", displayCommits)
+  req.open("GET", `https://api.github.com/repos/${username}/${name}/commits`)
+  req.send()
 }
 
 function displayCommits(){
   let commits = JSON.parse(this.responseText)
-  const commitList = `<ul>${commits.map(c =>
+
+  const commitList = `<ul${commits.map(c =>
     '<li>' +
       c.commit.author.name + '<br>' +
       c.author.login + '<br>' +
       c.commit.message +
     '</li>'
   ).join('')}</ul>`
+
   document.getElementById("details").innerHTML = commitList
 }
 
@@ -45,18 +51,20 @@ function getBranches(el){
   const name = el.dataset.repository
   const username = el.dataset.username
   const req = new XMLHttpRequest()
-  req.addEventListener("load", displayBranches);
-  req.open("GET", `https://api.github.com/repos/${username}/${name}/branches`);
-  req.send();
+
+  req.addEventListener("load", displayBranches)
+  req.open("GET", `https://api.github.com/repos/${username}/${name}/branches`)
+  req.send()
 }
 
 function displayBranches(){
-  let branches = JSON.parse(this.responseText);
+  let branches = JSON.parse(this.responseText)
   console.log(branches)
   const branchList = `<ul>${branches.map(b =>
     '<li>' +
       b.name + '<br>' +
     '</li>'
   ).join('')}</ul>`
-  document.getElementById("details").innerHTML = branchList;
+
+  document.getElementById("details").innerHTML = branchList
 }
